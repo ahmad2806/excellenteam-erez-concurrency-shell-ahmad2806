8a0ab89f635466252f0ebfc9dcda7a32d83f66ae
ahmad2806 <ahmadhashem2806@gmail.com>
2018-12-17 15:10:17 +0200

    MVP

---
+++ program2.c
@@ -0,0 +1,13 @@
+//
+// Created by ahmad on 17/12/2018.
+//
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+    int i = argc - 1, number = 1;
+
+    for (; i > 0; i--) {
+        printf("%d: %s\n", number, argv[i]);
+        number ++;
+    }
+    exit(0);
+}
